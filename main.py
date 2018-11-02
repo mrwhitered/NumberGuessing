52983da1c370708c6c456c048f88b131a3c1cd52
Furkan Akal <30433306+mrwhitered@users.noreply.github.com>
2018-11-02 21:31:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+#
+# 02.11.2018 friday
+#
+# coded by mrwhitered
+#
+
+from random import randint
+
+x = randint(1, 100)
+
+tahmin = False
+
+while not tahmin:
+    try:
+        Sayı = int(input("1 ile 100 arasında bir sayı girin: "))
+
+    except ValueError:
+        print("Geçerli bir sayı girin: ")
+
+        continue
+
+    if Sayı> x + 20 :
+        print("Çok büyük")
+
+    elif  Sayı>x:
+        print("Büyük")
+
+    elif Sayı  < x - 20  :
+        print("Çok küçük")
+
+    elif Sayı<x:
+        print("Küçük")
+
+    else:
+        tahmin = True
+
+print("Tebrikler")
